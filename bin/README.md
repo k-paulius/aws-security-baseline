b9913588f061afd2b4fb66cf606b2666cb589359
k-paulius <k.dash.paulius@gmail.com>
2024-05-26 13:44:18 -0500

    Adding script for designating the Security Hub administrator account for an organization

--- bin/README.md
+++ bin/README.md
@@ -16,6 +16,11 @@
     - Register a delegated administrator account for AWS Config and/or AWS Config Multi-Account Setup.
     - Usage: `./enable_delegated_admin_for_config.sh 123456789012`
 
+- `enable_delegated_admin_for_security_hub.sh`
+    - Designate the Security Hub administrator account for an organization.
+        - Also enables AWS Security Hub as a Trusted Service in Organizations.
+    - Usage: `./enable_delegated_admin_for_security_hub.sh 123456789012`
+
 - `enable_trusted_access_for_access_analyzer.sh`
     - Enables AWS IAM Access Analyzer as a Trusted Service in Organizations.
     - Usage: `./enable_trusted_access_for_access_analyzer.sh`
---
+++ bin/enable_delegated_admin_for_security_hub.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+DEL_ADMIN_ACCOUNT_ID=$1
+
+if [ -z $DEL_ADMIN_ACCOUNT_ID ]; then
+    echo "ERROR: No delegated administrator account ID argument supplied."
+	echo "Usage: $0 <delegated_admin_account_id>"
+	exit 1
+fi
+
+ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+if [ -z $ACCOUNT_ID ]; then
+    exit
+fi
+ACCOUNT_ALIAS=$(aws iam list-account-aliases --query 'AccountAliases' --output text)
+echo "- Operating on account $ACCOUNT_ID | $ACCOUNT_ALIAS."
+
+echo "- Checking if account $DEL_ADMIN_ACCOUNT_ID is already registered as a delegated administrator..."
+DELEGATION_DATE=$(aws organizations list-delegated-administrators \
+                      --service-principal securityhub.amazonaws.com \
+                      --query "DelegatedAdministrators[?Id == '$DEL_ADMIN_ACCOUNT_ID'].DelegationEnabledDate" \
+                      --output text)
+
+if [ -n "$DELEGATION_DATE" ]; then
+    echo "- Account $DEL_ADMIN_ACCOUNT_ID is already registered as a delegated administrator since $DELEGATION_DATE."
+else
+    echo "- Account $DEL_ADMIN_ACCOUNT_ID is not registered as a delegated administrator."
+    echo "- NOTE:"
+    echo "   This will enable AWS Security Hub as a Trusted Service in Organizations, register a delegated administrator"
+    echo "   account for Security Hub in Organizations and designate the Security Hub administrator account in this region."
+
+    read -p "Do you want to register account $DEL_ADMIN_ACCOUNT_ID as a delegated administrator? [y/N]" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "- Registering account $DEL_ADMIN_ACCOUNT_ID as a delegated administrator..."
+        aws securityhub enable-organization-admin-account \
+            --admin-account-id=$DEL_ADMIN_ACCOUNT_ID
+        echo "- Done."
+    else
+        echo "- Exiting."
+    fi
+fi
